025cd8d62094d3185ca2203c756c19159a47feca
Maksudur Rahman Maateen <maateen@outlook.com>
2020-06-02 10:40:32 +0600

    fixing error and adding cron build support
    Signed-off-by: Maksudur Rahman Maateen <maateen@outlook.com>

--- buildnpush.sh
+++ buildnpush.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
 repo_name="maateen/docker-beanstalkd"
-alpine_versions=('edge' 'latest' '3.12' '3.11' '3.10' '3.9')
-beanstalkd_versions=('master' '1.11' '1.10' '1.9' '1.8')
+alpine_versions=('3.9' '3.10' '3.11' '3.12' 'latest' 'edge')
+beanstalkd_versions=('1.8' '1.9' '1.10' '1.11')
+
+buildNpush() {
+    alpine_version=$1
+    beanstalkd_version=$2
+    echo "Building beanstalkd-$beanstalkd_version on alpine:$alpine_version"
+    docker build -t $repo_name:$beanstalkd_version-alpine-$alpine_version --build-arg ALPINE_VERSION=$alpine_version --build-arg BEANSTALKD_VERSION=$beanstalkd_version .
+    docker push $repo_name:$beanstalkd_version-alpine-$alpine_version
+
+    if [[ "${alpine_version}" == "edge" ]] && [[ "${beanstalkd_version}" == "master" ]]; then
+        docker tag $repo_name:$beanstalkd_version-alpine-$alpine_version $repo_name:latest
+        docker push $repo_name:latest
+    fi
+}
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-for alpine_version in "${alpine_versions[@]}"; do
-    for beanstalkd_version in "${beanstalkd_versions[@]}"; do
-        echo "Building beanstalkd-$beanstalkd_version on alpine:$alpine_versions"
-        docker build -t $repo_name:$beanstalkd_version-alpine-$alpine_versions --build-arg ALPINE_VERSION=$alpine_version --build-arg BEANSTALKD_VERSION=$beanstalkd_version .
-        docker push $repo_name:$beanstalkd_version-alpine-$alpine_versions
+if [[ "${TRAVIS_EVENT_TYPE}" == "cron" ]]; then
+    beanstalkd_version="master"
+    for alpine_version in "${alpine_versions[@]}"; do
+        buildNpush $alpine_version $beanstalkd_version
     done
-done
+else
+    for alpine_version in "${alpine_versions[@]}"; do
+        for beanstalkd_version in "${beanstalkd_versions[@]}"; do
+            buildNpush $alpine_version $beanstalkd_version
+        done
+    done
+fi
